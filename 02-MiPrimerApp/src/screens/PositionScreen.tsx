145d0527811cfbf03c44e5816df412cbdb8236a1
MatiasHamie <a445835@santandertecnologia.com.ar>
2021-09-15 13:11:04 -0300

    practicando posicion absoluta

--- 02-MiPrimerApp/src/screens/PositionScreen.tsx
+++ 02-MiPrimerApp/src/screens/PositionScreen.tsx
@@ -6,6 +6,7 @@ export const PositionScreen = () => {
     <View style={styles.container}>
       <View style={styles.cajaMorada} />
       <View style={styles.cajaNaranja} />
+      <View style={styles.cajaVerde} />
     </View>
   );
 };
@@ -25,6 +26,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856d6',
     borderWidth: 10,
     borderColor: '#fff',
+
+    // practicando pos absolute
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
   },
   cajaNaranja: {
     width: 100,
@@ -32,5 +38,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0a23b',
     borderWidth: 10,
     borderColor: '#fff',
+
+    // practicando pos absolute
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  cajaVerde: {
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: '#fff',
+    // width: 100,
+    // height: 100,
+
+    // practicando pos absolute
+    // si yo pongo todo en cero, y no defino una medida,
+    // la caja se va a estirar a todo lo q mida el padre
+    // el sinonimo de eso en react native es
+    // StyleSheet.absoluteFillObject
+    // position: 'absolute',
+    // top: 0,
+    // right: 0,
+    // left: 0,
+    // bottom: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });
